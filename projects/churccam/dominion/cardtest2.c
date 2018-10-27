9f46050546c8b09259d3967355322683b2f81bff
Cameron Church <churccam@oregonstate.edu>
2018-10-27 15:00:12 -0700

    cardtest2.c

---
+++ projects/churccam/dominion/cardtest2.c
@@ -0,0 +1,63 @@
+/*
+ * cardtest2.c
+ *
+ * Description: unit tests for adventurer card
+ *
+ * Include the following lines in your makefile:
+ *
+ * cardtest2: cardtest2.c dominion.o rngs.o
+ *      gcc -o cardtest2 -g  cardtest2.c dominion.o rngs.o $(CFLAGS)
+ */
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stddef.h>
+
+#define TEST_CARD "smithy"
+
+int main(int argc, char** argv) {
+    int seed = 100;
+    int numPlayers = 2;
+    int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+                 sea_hag, tribute, smithy, council_room};
+
+    //initialize game state
+    struct gameState default_GS, test_GS;
+
+    initializeGame(numPlayers, k, seed, &test_GS);
+
+
+    printf("\n--------------- Testing Card: %s ---------------\n", TEST_CARD);
+
+
+}
+
+/*Original adventurer card effect
+ * switch( card )
+    {
+    case adventurer:
+      while(drawntreasure<2){
+	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+	  shuffle(currentPlayer, state);
+	}
+	drawCard(currentPlayer, state);
+	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+	  drawntreasure++;
+	else{
+	  temphand[z]=cardDrawn;
+	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+	  z++;
+	}
+      }
+      while(z-1>=0){
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	z=z-1;
+      }
+      return 0;
+ */
